a32dff261d6e7ed75ca7dd2a1de97af96edb36d3
Anovikov <dang3rouz@yandex.ru>
2019-12-30 13:26:49 +0500

    updated app to redirect to home if user signed in

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
-ReactDOM.render( 
-    <BrowserRouter>
-    <App> </App> 
-    </BrowserRouter>, 
+import store from './redux/store'
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App> </App>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
 
