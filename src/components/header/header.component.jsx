fa6ae0684d1e114d3408e352db84a54b848f6417
Anovikov <dang3rouz@yandex.ru>
2020-01-12 13:37:59 +0500

    added checkout page, checkout item

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -44,7 +44,7 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 )
 
-const mapStateToProps = (state) => createStructuredSelector({
+const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser,
     hidden: selectCartHidden
 })
