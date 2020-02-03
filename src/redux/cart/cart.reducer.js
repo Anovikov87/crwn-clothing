146abb3497f1621e426c8ea7f63dabc7ffe3ed39
Anovikov <dang3rouz@yandex.ru>
2020-02-03 18:08:07 +0500

    created a lot of sagas

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,5 @@
 import CartActionTypes from './cart.types'
 import { addItemToCart, removeItemFromCart } from './cart.utils'
-import CartItem from '../../components/cart-item/cart-item.component'
 
 const INITIAL_STATE = {
     hidden: true,
@@ -30,6 +29,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 cartItems: state.cartItems.filter(
                     cartItem => cartItem.id != action.payload.id
                 )
+            }
+        case CartActionTypes.CLEAR_CART:
+            return {
+                ...state,
+                cartItems: []
             }
         default:
             return state
